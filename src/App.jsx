5f01d08389767bc81b862655e06db5c5792dc78b
gabriel.perez <gabperez.exito@gmail.com>
2026-09-06 16:32:30 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Saludo from './Saludo.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <h2>Usando React sin IA</h2>
+      <br>
+      </br>
+      <Saludo nombre="Gabriel" />
+      <Saludo nombre="Diana" />
+      <Saludo nombre="Opinator" />
+      <Saludo/>
+      <Saludo nombre={8656754} edad={42} activo={true}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Saludo.jsx
@@ -0,0 +1,21 @@
+/*
+//componente con props
+function Saludo(props){
+    return(
+        <h1><p>Hola, {props.nombre}</p></h1>
+    )
+}
+*/
+
+//Otra forma de hacer el componente Saludo es usando destructuring en los props y asignando valores por defecto.
+function Saludo({nombre = "invitado", edad, activo}){
+    return(
+        <p>
+            <h1>Hola, {nombre}</h1>
+            {edad && <h2>Edad: {edad}</h2>}
+            {activo && <h3>Cuenta activa. {activo}</h3>}
+        </p>
+    )
+}
+
+export default Saludo
