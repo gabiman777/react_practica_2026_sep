7097def92d554f3ebc6eabc58a25dcc3d813794a
gabriel.perez <gabperez.exito@gmail.com>
2026-09-06 16:46:33 -0300

    removed line saludo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
       <h2>Usando React sin IA</h2>
       <br>
       </br>
-      <Saludo nombre="Gabriel" />
       <Saludo nombre="Diana" />
       <Saludo nombre="Opinator" />
       <Saludo/>
